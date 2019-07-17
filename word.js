d11c1ff8967f8b2c4a0bdfa46aa2c37a67ab0322
MarcoTucsonpo <martucazpo@gmail.com>
2019-07-16 20:24:31 -0700

    word and letter ok

--- word.js
+++ word.js
@@ -3,22 +3,22 @@ var LetterObject = require("./letter.js");
 var letterObject = new LetterObject();
 
 var wordArray = ["mongoose", "shrew", "possum", "aardvark"];
-var randNum = Math.floor(Math.random() * wordArray.length);
-var randWord = wordArray[randNum];
-var chars = JSON.stringify(randWord).split('');
-var cleanWord = chars.slice(1, chars.length - 1);
-var letter;
-
 
 var Word = function () {
+
+  var randNum = Math.floor(Math.random() * wordArray.length);
+  var randWord = wordArray[randNum];
+  var chars = JSON.stringify(randWord).split('');
+  var cleanWord = chars.slice(1, chars.length - 1);
+  var letter;
   var gameWord = [];
+
   this.makeWord = function () {
     console.log(cleanWord);
     for (i = 0; i < cleanWord.length; i++) {
       letter = cleanWord[i];
       letterObject.letter = letter;
-      gameWord.push(letterObject);
-      gameWord[i].display();
+      gameWord.push(letterObject.display());
     }
   };
   makeWord();
